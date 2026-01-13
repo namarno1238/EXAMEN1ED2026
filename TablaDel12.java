59f1036ca5824f4b6df5e0b87a864db36129f98c
juanillo <juanillo@gmail.com>
2026-01-13 13:35:06 +0100

    cambio 1 juan

--- TablaDel12.java
+++ TablaDel12.java
@@ -6,5 +6,8 @@ public class TablaDel12
         {
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
+        {
+            System.out.println("es la tabla del 12");
+        }
     }
 }
